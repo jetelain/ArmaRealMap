04ac0ea64f9e57455a4ecfafe20f6e8addc8572c
Julien <julien.etelain@outlook.com>
2021-04-24 10:02:14 +0200

    Base config

---
+++ Belfort/@ArmBelfort/addons/belfort/config.cpp
@@ -0,0 +1,70 @@
+class CfgPatches
+{
+	class arm_belfort
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1;
+		requiredAddons[] =
+		{
+			"A3_Map_Stratis"
+		};
+	};
+};
+ 
+class CfgWorldList
+{
+	class arm_belfort{};
+};
+ 
+class CfgWorlds
+{
+	class Stratis;
+	class arm_belfort: Stratis
+	{
+		cutscenes[] = {};
+		description = "BELFORT, France";
+		worldName = "z\arm\addons\belfort\belfort.wrp";
+		author = "GrueArbre";
+		icon = "";
+		previewVideo = "";
+		pictureMap = "";
+		pictureShot = "";
+ 
+		newRoadsShape = "";
+ 
+		centerPosition[] =
+		{
+			10240, 10240
+		};
+		ilsDirection[] =
+		{
+			0, 0.08, 1
+		};
+		ilsPosition[] =
+		{
+			0, 0
+		};
+		ilsTaxiIn[] = {};
+		ilsTaxiOff[] = {};
+		drawTaxiway = 0;
+		class SecondaryAirports{};
+		class ReplaceObjects{};
+ 
+		class Sounds
+		{
+			sounds[] = {};
+		};
+ 
+		class Animation
+		{
+			vehicles[] = {};
+		};
+ 
+		minTreesInForestSquare = 2;
+		minRocksInRockSquare = 2;
+ 
+		class Subdivision{};
+		class Names{};
+	};
+};
