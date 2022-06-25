37987b0c84762bc0c86457ae601d9d6757563cb4
Dabako <dabako@gmx.de>
2022-06-25 14:35:41 +0200

    Use Variable for file directory

--- make.cmd
+++ make.cmd
@@ -1,3 +1,3 @@
-"C:\Users\Julien\source\repos\jetelain\bis-file-formats\Utils\WrpUtil\bin\Debug\net5.0\wrputil.exe" edit c:\temp\gossi.wrp P:\z\arm\addons\common\data\trees\scale.csv P:\z\arm\addons\gossi\gossi.wrp
+"C:\Users\%username%\source\repos\jetelain\bis-file-formats\Utils\WrpUtil\bin\Debug\net5.0\wrputil.exe" edit c:\temp\gossi.wrp P:\z\arm\addons\common\data\trees\scale.csv P:\z\arm\addons\gossi\gossi.wrp
 "C:\Program Files (x86)\Mikero\DePboTools\bin\pboProject.exe" -E="arma3" -W -M="C:\temp\@ArmGossi" -P P:\z\arm\addons\gossi
 pause
