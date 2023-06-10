78abf61ee9eb86ef0e5efc002d4f406d226152a7
Julien <julien.etelain@outlook.com>
2023-06-10 16:43:37 +0200

    Supports files as command line arguments

--- GameRealisticMap.Studio/Modules/Main/MainModule.cs
+++ GameRealisticMap.Studio/Modules/Main/MainModule.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.IO.Packaging;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using Caliburn.Micro;
-using GameRealisticMap.Studio.Modules.Explorer.ViewModels;
 using GameRealisticMap.Studio.Modules.Main.ViewModels;
 using Gemini.Framework;
 using Gemini.Framework.Services;
@@ -29,14 +26,29 @@ namespace GameRealisticMap.Studio.Modules.Main
         {
             _mainWindow.Title = "GameRealisticMap Studio";
             _mainWindow.Icon = new BitmapImage(new("pack://application:,,,/GameRealisticMap.Studio;component/Resources/Icons/MainWindowIcon.png"));
-            //>>>PackageStore.GetPackage(new ("application:///"));
             _mainWindow.Shell.ToolBars.Visible = true;
         }
 
         public override async Task PostInitializeAsync()
         {
             await _mainWindow.Shell.OpenDocumentAsync(IoC.Get<HomeViewModel>());
-        }
 
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                var providers = IoC.GetAllInstances(typeof(IEditorProvider)).Cast<IEditorProvider>();
+
+                foreach (var path in args.Skip(1).Where(str => File.Exists(str)))
+                {
+                    var provider = providers.FirstOrDefault(p => p.Handles(path));
+                    if (provider!= null)
+                    {
+                        var editor = provider.Create();
+                        await provider.Open(editor, path);
+                        await _mainWindow.Shell.OpenDocumentAsync(editor);
+                    }
+                }
+            }
+        }
     }
 }
