5bfb5440c44df52138fa0c90a0f8d5f9d6a68097
Julien <julien.etelain@outlook.com>
2023-09-16 10:38:02 +0200

    Contours preview display performance

--- GameRealisticMap/ElevationModel/ElevationContourBuilder.cs
+++ GameRealisticMap/ElevationModel/ElevationContourBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using GameRealisticMap.Geometries;
 using GameRealisticMap.Reporting;
-using GeoAPI.Geometries;
 using MapToolkit.Contours;
 
 namespace GameRealisticMap.ElevationModel
@@ -24,9 +23,23 @@ namespace GameRealisticMap.ElevationModel
                 contour.Add(elevation.ToDataCell(), new ContourLevelGenerator(-50, 5), false, report);
             }
 
-            var paths = contour.Lines.Select(l => new TerrainPath(l.Points.Select(p => new TerrainPoint((float)p.Longitude, (float)p.Latitude)).ToList())).ToList();
+            var paths = contour.Lines.ProgressStep(progress,"Lines").Select(l => new TerrainPath(
+                TerrainPath.Simplify(l.Points.Select(p => new TerrainPoint((float)p.Longitude, (float)p.Latitude)).ToList()))).ToList();
+
+            var slices = CreateSlices(context.Area.TerrainBounds).ToList();
+
+            paths = slices.SelectManyParallel(progress, "Slices", s => paths.SelectMany(p => p.ClippedByEnveloppe(s))).ToList();
 
             return new ElevationContourData(paths);
+        }
+
+        private IEnumerable<ITerrainEnvelope> CreateSlices(ITerrainEnvelope envelope)
+        {
+            if ( envelope.MaxPoint.X - envelope.MinPoint.X > 2000 )
+            {
+                return envelope.SplitQuad().SelectMany(CreateSlices);
+            }
+            return new[] { envelope };
         }
     }
 }
