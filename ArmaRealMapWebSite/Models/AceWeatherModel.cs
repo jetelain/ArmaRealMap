412b3cab97977365c8bf94f0f4cb0c74b6239c3e
Julien <julien.etelain@outlook.com>
2023-10-03 12:39:53 +0200

    Give access to ACE3 Weather tool online + Update ARM site

---
+++ ArmaRealMapWebSite/Models/AceWeatherModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArmaRealMapWebSite.Models
+{
+    public class AceWeatherModel
+    {
+        [Required]
+        [Display(Description = "Coordinates")]
+        public string Coordinates { get; set; }
+
+
+        public string AceConfig { get; set; }
+    }
+}
