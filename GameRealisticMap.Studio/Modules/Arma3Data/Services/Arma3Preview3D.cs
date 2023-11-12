a84c0834a3466fe12c76d57093927cfe9f177a07
Julien <julien.etelain@outlook.com>
2023-11-12 11:15:12 +0100

    Move code to a specific service

---
+++ GameRealisticMap.Studio/Modules/Arma3Data/Services/Arma3Preview3D.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using BIS.P3D.ODOL;
+
+namespace GameRealisticMap.Studio.Modules.Arma3Data.Services
+{
+    [Export(typeof(IArma3Preview3D))]
+    internal sealed class Arma3Preview3D : IArma3Preview3D
+    {
+        private readonly IArma3DataModule _arma3DataModule;
+        private readonly IArma3Previews _arma3Previews;
+
+        [ImportingConstructor]
+        public Arma3Preview3D(IArma3DataModule arma3DataModule, IArma3Previews arma3Previews)
+        {
+            _arma3DataModule = arma3DataModule;
+            _arma3Previews = arma3Previews;
+        }
+
+        public Model3DGroup? GetModel(string path)
+        {
+            var p3d = _arma3DataModule.Library.ReadODOL(path);
+            if (p3d == null)
+            {
+                return null;
+            }
+
+            var lods = p3d.Lods.Where(l => l.FaceCount > 0 && l.Resolution < 1000).OrderBy(l => l.Resolution).ToList();
+            var visualLod = /*lods.FirstOrDefault(l => !l.Materials.Any(m => m.PixelShader == 103)) ??*/ lods.FirstOrDefault();
+            if (visualLod == null)
+            {
+                return null;
+            }
+
+            return ToModel3D(visualLod);
+        }
+
+        private Model3DGroup ToModel3D(LOD visualLod)
+        {
+            var positions = GetPositions(visualLod);
+            var textureCoordinates = GetTextureCoordinates(visualLod);
+            var group = new Model3DGroup();
+            //var normals = new Vector3DCollection(visualLod.Normals != null ? visualLod.Normals.Select(n => new Vector3D(n.X, n.Y, n.Z)) : visualLod.NormalsCompressed.Select(n => new Vector3D(n.X, n.Y, n.Z)));
+
+            foreach (var section in visualLod.Sections)
+            {
+                var material = GetMaterial(visualLod, section);
+                if (material != null)
+                {
+                    var mesh = new MeshGeometry3D();
+                    mesh.Positions = positions;
+                    mesh.TextureCoordinates = textureCoordinates;
+                    // mesh.Normals = normals;
+                    mesh.TriangleIndices = new Int32Collection(section.GetFaces(visualLod.Polygons.Faces).Reverse().SelectMany(GetTriangeIndices));
+                    var faceModel = new GeometryModel3D(mesh, material);
+                    group.Children.Add(faceModel);
+                }
+            }
+
+            return group;
+        }
+
+        private static IEnumerable<int> GetTriangeIndices(Polygon f)
+        {
+            if (f.VertexIndices.Length == 3)
+            {
+                return f.VertexIndices;
+            }
+            return f.VertexIndices.Take(3).Concat(f.VertexIndices.Skip(2).Concat(f.VertexIndices.Take(1)));
+        }
+
+        private static Point3DCollection GetPositions(LOD visualLod)
+        {
+            return new Point3DCollection(
+                visualLod.Vertices.Select(v => v.Vector3).Select(v => new Point3D(v.X, v.Y, v.Z)).Concat(
+                    new[] { new Point3D(0, 0, 0), new Point3D(0, 0, 0) }
+                    ));
+        }
+
+        private PointCollection GetTextureCoordinates(LOD visualLod)
+        {
+            var uvset = visualLod.UvSets[0];
+            var textureCoordinates = new PointCollection(uvset.GetUV().Select(c => new System.Windows.Point(ClampUV(c.X), ClampUV(c.Y))).Concat(
+                new[] { new System.Windows.Point(0, 0), new System.Windows.Point(1, 1) }
+                ));
+            return textureCoordinates;
+        }
+
+        private Material? GetMaterial(LOD visualLod, Section section)
+        {
+            string texture = string.Empty;
+            if (section.TextureIndex != -1)
+            {
+                texture = visualLod.Textures[section.TextureIndex];
+            }
+            if (section.MaterialIndex != -1 && string.IsNullOrEmpty(texture))
+            {
+                texture = visualLod.Materials[section.MaterialIndex].StageTextures[0].Texture;
+            }
+            var file = _arma3Previews.GetTexturePreview(texture);
+            if (file != null)
+            {
+                return new DiffuseMaterial(new ImageBrush(new BitmapImage(file)));
+            }
+            return new DiffuseMaterial(new SolidColorBrush(Colors.DarkRed));
+        }
+
+        private double ClampUV(float v)
+        {
+            if (v >= 0)
+            {
+                return v % 1f;
+            }
+            return (v % 1f) + 1f;
+        }
+    }
+}
--- GameRealisticMap.Studio/Modules/AssetBrowser/ViewModels/PreviewToolViewModel.cs
+++ GameRealisticMap.Studio/Modules/AssetBrowser/ViewModels/PreviewToolViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.ComponentModel.Composition;
 using System.Windows.Media.Media3D;
-using BIS.P3D.ODOL;
-using Caliburn.Micro;
-using GameRealisticMap.Studio.Modules.Arma3Data;
+using GameRealisticMap.Studio.Modules.Arma3Data.Services;
 using Gemini.Framework;
 using Gemini.Framework.Services;
 
@@ -17,101 +11,21 @@ namespace GameRealisticMap.Studio.Modules.AssetBrowser.ViewModels
     {
         public override PaneLocation PreferredLocation => PaneLocation.Right;
 
-        private readonly IArma3DataModule _arma3DataModule;
-        private readonly IArma3Previews _arma3Previews;
+        private readonly IArma3Preview3D _arma3Preview3D;
 
         [ImportingConstructor]
-        public PreviewToolViewModel(IArma3DataModule arma3DataModule, IArma3Previews arma3Previews)
+        public PreviewToolViewModel(IArma3Preview3D arma3Preview3D)
         {
-            _arma3DataModule = arma3DataModule;
-            _arma3Previews = arma3Previews;
+            _arma3Preview3D = arma3Preview3D;
             DisplayName = "3D Preview";
         }
 
         public void SetP3d(string path)
         {
-            Model3DGroup = null;
-
-            var p3d = _arma3DataModule.Library.ReadODOL(path);
-            if (p3d != null)
-            {
-                var lods = p3d.Lods.Where(l => l.FaceCount > 0 && l.Resolution < 1000).OrderBy(l => l.Resolution).ToList();
-                var visualLod = lods.FirstOrDefault(l => !l.Materials.Any(m => m.PixelShader == 103)) ?? lods.FirstOrDefault();
-                if (visualLod != null)
-                {
-                    var positions = GetPositions(visualLod);
-                    var textureCoordinates = GetTextureCoordinates(visualLod);
-                    var group = new Model3DGroup();
-                    //var normals = new Vector3DCollection(visualLod.Normals != null ? visualLod.Normals.Select(n => new Vector3D(n.X, n.Y, n.Z)) : visualLod.NormalsCompressed.Select(n => new Vector3D(n.X, n.Y, n.Z)));
-
-                    foreach (var section in visualLod.Sections)
-                    {
-                        var material = GetMaterial(visualLod, section);
-                        var mesh = new MeshGeometry3D();
-                        mesh.Positions = positions;
-                        mesh.TextureCoordinates = textureCoordinates;
-                        // mesh.Normals = normals;
-                        mesh.TriangleIndices = new Int32Collection(section.GetFaces(visualLod.Polygons.Faces).SelectMany(GetTriangeIndices));
-                        var faceModel = new GeometryModel3D(mesh, material) { BackMaterial = material };
-                        group.Children.Add(faceModel);
-                    }
-                    Model3DGroup = group;
-                }
-            }
+            Model3DGroup = _arma3Preview3D.GetModel(path);
             NotifyOfPropertyChange(nameof(Model3DGroup));
         }
 
-        private static IEnumerable<int> GetTriangeIndices(Polygon f)
-        {
-            if (f.VertexIndices.Length == 3)
-            {
-                return f.VertexIndices;
-            }
-            return f.VertexIndices.Take(3).Concat(f.VertexIndices.Skip(2).Concat(f.VertexIndices.Take(1)));
-        }
-
-        private static Point3DCollection GetPositions(LOD visualLod)
-        {
-            return new Point3DCollection(
-                visualLod.Vertices.Select(v => v.Vector3).Select(v => new Point3D(v.X, v.Y, v.Z)).Concat(
-                    new[] { new Point3D(0, 0, 0), new Point3D(0, 0, 0) }
-                    ));
-        }
-
-        private PointCollection GetTextureCoordinates(LOD visualLod)
-        {
-            var uvset = visualLod.UvSets[0].GetUV();
-            var textureCoordinates = new PointCollection(uvset.Select(c => new System.Windows.Point(Clamp(c.X), Clamp(c.Y))).Concat(
-                new[] { new System.Windows.Point(0, 0), new System.Windows.Point(1, 1) }
-                ));
-            return textureCoordinates;
-        }
-
-        private Material GetMaterial(LOD visualLod, Section section)
-        {
-            string texture = string.Empty;
-            if (section.TextureIndex != -1)
-            {
-                texture = visualLod.Textures[section.TextureIndex];
-            }
-            var file = _arma3Previews.GetTexturePreview(texture);
-            if (file != null)
-            {
-                return new DiffuseMaterial(new ImageBrush(new BitmapImage(file)) { Stretch = Stretch.Fill }); 
-            }
-            return new DiffuseMaterial(new SolidColorBrush(Colors.DarkRed));
-        }
-
-        private double Clamp(float v)
-        {
-            if ( v >= 0 )
-            {
-                return v % 1f;
-            }
-            return (v % 1f) + 1f;
-        }
-
         public Model3DGroup? Model3DGroup {  get; set; }
-
     }
 }
