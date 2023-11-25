e885f5f6cc6c9b72a346f430947dd19498756e82
Julien <julien.etelain@outlook.com>
2023-11-25 14:59:14 +0100

    Import elevation

---
+++ GameRealisticMap.Studio/Modules/Arma3WorldEditor/ViewModels/Import/ImportElevationTask.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using GameRealisticMap.Arma3.Edit;
+using GameRealisticMap.Arma3.GameEngine;
+using GameRealisticMap.ElevationModel;
+using GameRealisticMap.Studio.Modules.Reporting;
+
+namespace GameRealisticMap.Studio.Modules.Arma3WorldEditor.ViewModels.Import
+{
+    internal class ImportElevationTask : IProcessTask
+    {
+        private readonly Arma3WorldEditorViewModel parent;
+        private readonly ElevationGrid grid;
+        private readonly bool isUpdateElevation;
+
+        public ImportElevationTask(Arma3WorldEditorViewModel parent, ElevationGrid grid, bool isUpdateElevation)
+        {
+            this.parent = parent;
+            this.grid = grid;
+            this.isUpdateElevation = isUpdateElevation;
+        }
+
+        public string Title => "Import elevation";
+
+        public bool Prompt => false;
+
+        public Task Run(IProgressTaskUI ui)
+        {
+            var world = parent.World;
+            if (world == null)
+            {
+                return Task.CompletedTask;
+            }
+            var worker = new WrpEditProcessor(ui);
+            if (isUpdateElevation)
+            {
+                worker.UpdateElevationGrid(world, grid);
+            }
+            else
+            {
+                worker.UpdateElevationGridAbsolute(world, grid);
+            }
+            parent.PostEdit();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ GameRealisticMap.Studio/Modules/Arma3WorldEditor/ViewModels/Import/ModalProgressBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gemini.Framework;
+
+namespace GameRealisticMap.Studio.Modules.Arma3WorldEditor.ViewModels.Import
+{
+    internal abstract class ModalProgressBase : WindowBase, IProgress<double>
+    {
+        private bool _isWorking;
+        private double _workingPercent;
+        private string _error = string.Empty;
+
+        public Task Cancel() => TryCloseAsync(false);
+
+        public bool IsWorking
+        {
+            get { return _isWorking; }
+            set { _isWorking = value; NotifyOfPropertyChange(); NotifyOfPropertyChange(nameof(IsNotValid)); NotifyOfPropertyChange(nameof(IsValid)); }
+        }
+
+        public double WorkingPercent
+        {
+            get { return _workingPercent; }
+            set { _workingPercent = value; NotifyOfPropertyChange(); }
+        }
+
+        public void Report(double value)
+        {
+            WorkingPercent = value;
+        }
+
+        public string Error
+        {
+            get { return _error; }
+            set { _error = value; NotifyOfPropertyChange(); if (!_isWorking) { NotifyOfPropertyChange(nameof(IsNotValid)); NotifyOfPropertyChange(nameof(IsValid)); } }
+        }
+
+        public bool IsNotValid => !IsWorking && !string.IsNullOrEmpty(Error);
+
+        public bool IsValid => !IsWorking && string.IsNullOrEmpty(Error);
+
+    }
+}
