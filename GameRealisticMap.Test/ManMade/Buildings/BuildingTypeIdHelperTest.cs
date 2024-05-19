ee9b1aad372be7ce1b8ce6ed8d203577029e5f62
Julien <julien.etelain@outlook.com>
2024-05-19 19:40:30 +0200

    Unit test

---
+++ GameRealisticMap.Test/ManMade/Buildings/BuildingTypeIdHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameRealisticMap.ManMade.Buildings;
+using OsmSharp.Tags;
+
+namespace GameRealisticMap.Test.ManMade.Buildings
+{
+    public class BuildingTypeIdHelperTest
+    {
+
+        [Fact]
+        public void FromOSM()
+        {
+            Assert.Equal(BuildingTypeId.Agricultural, BuildingTypeIdHelper.FromOSM(new TagsCollection() { new Tag("building", "cowshed") } ));
+            Assert.Equal(BuildingTypeId.Residential, BuildingTypeIdHelper.FromOSM(new TagsCollection() { new Tag("building", "terrace") }));
+            Assert.Null(BuildingTypeIdHelper.FromOSM(new TagsCollection() { new Tag("building", "unknown-value") }));
+            Assert.Null(BuildingTypeIdHelper.FromOSM(new TagsCollection()));
+        }
+    }
+}
